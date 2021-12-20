5df9042209f49513895c74faa49b3bd858950b2d
Abdulrahman1890 <abdulrahmanmm2011@hotmail.com>
2021-12-21 00:10:27 +0200

    getstarted

--- app/src/main/java/com/example/evara/GetStarted.java
+++ app/src/main/java/com/example/evara/GetStarted.java
@@ -2,26 +2,46 @@ package com.example.evara;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class GetStarted extends AppCompatActivity {
-    ImageView slideImg;
-    AnimationDrawable animationDrawable;
-    Animation animFade;
+
+    Button getStarted;
+    TextView signIn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_started);
 
-        slideImg = findViewById(R.id.get_started_slide_img);
-        animFade = AnimationUtils.loadAnimation(this, R.anim.evara_faded);
-        slideImg.startAnimation(animFade);
-        animationDrawable = (AnimationDrawable) slideImg.getDrawable();
-        animationDrawable.start();
+        getSupportActionBar().hide();
+
+        getStarted = findViewById(R.id.getstarted_btn);
+        signIn = findViewById(R.id.getstarted_tv_signin);
+
+        getStarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(GetStarted.this,Register.class);
+                startActivity(intent);
+
+            }
+        });
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/evara/Register.java
+++ app/src/main/java/com/example/evara/Register.java
@@ -3,13 +3,28 @@ package com.example.evara;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Register extends AppCompatActivity {
+
+    private EditText userName,email,password,confirmPassword;
+    private Button register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        getSupportActionBar().hide();
+
+        userName = findViewById(R.id.register_edt_username);
+        email = findViewById(R.id.register_edt_email);
+        password = findViewById(R.id.register_edt_password);
+        confirmPassword = findViewById(R.id.register_edt_confirmpassword);
+        register = findViewById(R.id.regist_btn);
+
+
 
 
     }
